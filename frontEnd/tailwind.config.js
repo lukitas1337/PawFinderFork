53cb01bfd9c0ae7932bc46a9f7a4a30952a964cb
Pureschko <inna.shekina@gmail.com>
2024-11-15 20:10:42 +0100

    Added about us

--- frontEnd/tailwind.config.js
+++ frontEnd/tailwind.config.js
@@ -3,9 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
       colors: {
         dark: "#071327",
         green: "#BFCF59",
+        red: "#ED6656",
       },
     },
   },
