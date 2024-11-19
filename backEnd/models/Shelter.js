e48b71310efd2304a468e61c9b20b63b958395b7
Manuel <manuel.penner@mein.gmx>
2024-11-19 10:28:52 +0100

    add shelter.js and seperate user from shelters

---
+++ backEnd/models/Shelter.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const shelterSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    contactPerson: {
+      type: String,
+      required: true,
+      },
+    companyName: {
+      type: String,
+      required: true,
+    },
+    registrationNumber: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    capacity: {
+      type: Number,
+      required: true,
+    },
+    website: {
+      type: String,
+    },
+    capacity: {
+      type: Number, // Maximum number of animals the shelter can hold
+      required: true,
+    },
+    availablePets: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Pets",
+      },
+    ],
+    description: {
+      type: String,
+    },
+    companyImage: {
+      type: String,
+    },
+  },
+  {
+    collection: "Shelters",
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Shelter", shelterSchema);
--- backEnd/models/User.js
+++ backEnd/models/User.js
@@ -15,42 +15,34 @@ const userSchema = new mongoose.Schema(
     type: String,
     required: true,
   },
-  userType: {
-    type: String,
-    enum: ['individual', 'company'],
-    required: true,
-  },
-  companyName: {
-    type: String,
-    required: function() { return this.userType === 'company'; },
-  },
-  registrationNumber: {
-    type: String,
-    required: function() { return this.userType === 'company'; },
-  },
-  contactPerson: {
-    type: String,
-    required: function() { return this.userType === 'company'; },
-  },
   phoneNumber: {
     type: String,
     required: false,
-  },
-  roleId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Role',
-    required: true,
   },
   googleId: {
     type: String,
     unique: true,
     sparse: true,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  }
-}, { 
+  adoptionApplications: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Pet",
+    },
+  ],
+  questionnaires: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Questionnaire",
+    },
+  ],
+  },
+  {
+    timestamps: true,
+    collection: "Users",
+    versionKey: false
+  }, { 
+  timestamps: true,
   collection: 'Users',
   versionKey: false
 });
