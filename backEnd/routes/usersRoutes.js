4648519f5eb21588e8fbac21beada7d72deecd53
Lukitas <lvd.follert@gmail.com>
2024-11-19 13:55:07 +0100

    cleaned usersRoutes

--- backEnd/routes/usersRoutes.js
+++ backEnd/routes/usersRoutes.js
@@ -2,21 +2,11 @@ import { Router } from 'express';
 import {
   getUsers,
   getUserById,
-  createUser,
   updateUser,
   deleteUser,
-  loginUser,
-  logoutUser,
-  checkSession,
 } from '../controllers/usersControllers.js';
-import auth from '../middleware/authMiddleware.js';
 
 const usersRouter = Router();
-
-usersRouter.post('/register', createUser);
-usersRouter.post('/login', loginUser);
-usersRouter.post('/logout', logoutUser);
-usersRouter.get('/session', auth, checkSession);
 
 usersRouter.get('/', getUsers);
 usersRouter.get('/:id', getUserById);
