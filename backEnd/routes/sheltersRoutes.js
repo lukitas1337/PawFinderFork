12e9d15163827d6b6686dd41630b842c96da2f17
Manuel <manuel.penner@mein.gmx>
2024-11-20 11:57:04 +0100

    fixed shelterRoutes not working

--- backEnd/routes/sheltersRoutes.js
+++ backEnd/routes/sheltersRoutes.js
@@ -1,17 +1,19 @@
-import express from 'express';
+import { Router } from "express";
 // import auth from '../middleware/auth.js';
-import { getShelters, getShelterById, createShelter, updateShelter, deleteShelter } from '../controllers/shelterControllers.js';
+import {
+  getShelters,
+  getShelterById,
+  createShelter,
+  updateShelter,
+  deleteShelter,
+} from "../controllers/shelterControllers.js";
 
-const router = express.Router();
+const shelterRouter = Router();
 
-router.post('/', createShelter);
+shelterRouter.get("/", getShelters);
+shelterRouter.get("/:id", getShelterById);
+shelterRouter.post("/", createShelter);
+shelterRouter.patch("/:id", updateShelter);
+shelterRouter.delete("/:id", deleteShelter);
 
-router.patch('/:id', updateShelter);
-
-router.delete('/:id', deleteShelter);
-
-router.get('/', getShelters);
-
-router.get('/:id', getShelterById);
-
-export default router;
+export default shelterRouter;
