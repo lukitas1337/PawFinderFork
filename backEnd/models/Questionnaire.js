d6340a7e85352731fee225b9fce12fafe1c62c2e
Lukitas <lvd.follert@gmail.com>
2024-11-21 16:24:21 +0100

    test user and questionnaireModel

--- backEnd/models/Questionnaire.js
+++ backEnd/models/Questionnaire.js
@@ -1,47 +1,181 @@
 import mongoose from 'mongoose';
 
+const existingPetSchema = new mongoose.Schema({
+  petType: {
+    type: String,
+    enum: ['dog', 'cat', 'other'],
+    required: true
+  },
+  otherPetType: String,
+  count: Number,
+  ages: String,
+  genders: [{
+    type: String,
+    enum: ['male', 'male_neutered', 'female', 'female_spayed']
+  }],
+  characterDescription: String,
+  compatibility: {
+    type: String,
+    enum: ['compatible_same_species', 'compatible_other_species', 'not_compatible', 'unknown']
+  }
+});
+
 const questionnaireSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
-  preferredAnimalType: {
+  
+  housingSituation: {
     type: String,
-    enum: ['cat', 'dog', 'both'],
+    enum: ['apartment_shared_entrance', 'apartment_separate_entrance', 'house', 'mostly_outdoors'],
     required: true
   },
-  preferredAgeRange: {
-    min: {
-      type: Number,
-      required: true
-    },
-    max: {
-      type: Number,
-      required: true
-    }
-  },
-  homeType: {
-    type: String,
-    enum: ['apartment', 'house', 'house_with_yard'],
-    required: true
-  },
-  hasChildren: {
-    type: Boolean,
-    required: true
-  },
-  hasOtherPets: {
-    type: Boolean,
-    required: true
-  },
-  hoursHome: {
+
+  dailyAloneHours: {
     type: Number,
     required: true
+  },
+
+  adaptationPeriodDays: {
+    type: Number,
+    required: true
+  },
+
+  workplaceAccommodation: {
+    canTakePet: {
+      type: String,
+      enum: ['yes', 'no', 'not_necessary'],
+      required: true
+    },
+    workplaceHousingDetails: String
+  },
+
+  emergencyCare: {
+    type: String,
+    enum: ['yes', 'will_find_upon_adoption'],
+    required: true
+  },
+
+  householdComposition: {
+    type: String,
+    required: true
+  },
+
+  canSeparatePetDuringVisits: {
+    type: Boolean,
+    required: true
+  },
+
+  currentPets: {
+    hasPets: {
+      type: Boolean,
+      required: true
+    },
+    pets: [existingPetSchema]
+  },
+
+  petExperience: {
+    hasPreviousExperience: Boolean,
+    confidenceLevels: [{
+      type: String,
+      enum: [
+        'previous_ownership',
+        'multiple_pets_experience',
+        'confident_with_challenging_pets',
+        'understand_serious_behavioral_management'
+      ]
+    }]
+  },
+
+  specialRequirements: {
+    hasExperience: Boolean,
+    types: [{
+      type: String,
+      enum: [
+        'nervousness_restlessness',
+        'separation_anxiety',
+        'self_harming',
+        'fearfulness',
+        'strong_prey_drive',
+        'resource_guarding',
+        'aggression_humans',
+        'aggression_strangers',
+        'aggression_family',
+        'aggression_animals',
+        'other'
+      ]
+    }]
+  },
+
+  healthIssues: {
+    hasExperience: Boolean,
+    conditions: [{
+      type: String,
+      enum: [
+        'joint_issues',
+        'gastrointestinal',
+        'heart_disease',
+        'thyroid',
+        'kidney_disease',
+        'liver_disease',
+        'allergies',
+        'diabetes',
+        'other'
+      ]
+    }]
+  },
+
+  medicationAdministration: {
+    type: String,
+    enum: ['yes', 'need_consultation'],
+    required: true
+  },
+
+  trainingPlans: {
+    type: String,
+    enum: ['yes', 'no', 'depends'],
+    required: true
+  },
+
+  trainingMethodKnowledge: {
+    type: String,
+    enum: ['familiar', 'need_consultation'],
+    required: true
+  },
+
+  previousAdoption: {
+    hasAdopted: Boolean,
+    adoptedAnimals: String
+  },
+
+  petSurrender: {
+    hasSurrendered: Boolean,
+    surrenderReason: String
+  },
+
+  livingConditionsDescription: {
+    type: String,
+    required: true
+  },
+
+  additionalInformation: String,
+
+  legalConsiderations: {
+    rentalAgreementAcknowledged: {
+      type: Boolean,
+      required: true
+    },
+    allergyAcknowledged: {
+      type: Boolean,
+      required: true
+    }
   }
 }, {
   collection: 'Questionnaires',
-  versionKey: false,
-  timestamps: true
+  timestamps: true,
+  versionKey: false
 });
 
 export default mongoose.model('Questionnaire', questionnaireSchema);
--- backEnd/models/usersModel.js
+++ backEnd/models/usersModel.js
@@ -1,5 +1,53 @@
 import mongoose from "mongoose";
 
+// New Pet Preferences Schema
+const petPreferencesSchema = new mongoose.Schema({
+  petType: {
+    type: String,
+    enum: ['cat', 'dog', 'both'],
+    required: true
+  },
+  maxDistance: {
+    type: Number,  // in kilometers
+    min: 0,
+    required: true
+  },
+  ageRange: {
+    min: {
+      type: Number,  // in months
+      min: 0,
+      required: true
+    },
+    max: {
+      type: Number,  // in months
+      min: 0,
+      required: true
+    }
+  },
+  size: [{
+    type: String,
+    enum: ['small', 'medium', 'large'],
+    required: true
+  }],
+  gender: [{
+    type: String,
+    enum: ['male', 'male_neutered', 'female', 'female_spayed'],
+    required: true
+  }],
+  character: [{
+    type: String,
+    enum: [
+      'playful_energetic',    // Happy, always ready for activities
+      'calm_gentle',          // Peaceful and easy-going
+      'shy_reserved',         // Needs time to warm up
+      'independent_confident', // Self-assured, does well alone
+      'affectionate_cuddly',  // Loves physical contact
+      'social_friendly'       // Great with other animals/people
+    ],
+    required: true
+  }]
+});
+
 const userSchema = new mongoose.Schema(
   {
     email: {
@@ -7,57 +55,58 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-  password: {
-    type: String,
-    required: function() { return !this.googleId; },
-  },
-  fullName: {
-    type: String,
-    required: true,
-  },
-  userType: {
-    type: String,
-    enum: ['user', 'admin', 'shelter'],
-    default: 'user',
-    required: true,
-  },
-  phoneNumber: {
-    type: String,
-    required: false,
-  },
-  googleId: {
-    type: String,
-    unique: true,
-    sparse: true,
-  },
-  adoptionApplications: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Pet",
+    password: {
+      type: String,
+      required: function() { return !this.googleId; },
     },
-  ],
-  questionnaires: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Questionnaire",
+    fullName: {
+      type: String,
+      required: true,
     },
-  ],
-  myFavorites: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Pet",
+    userType: {
+      type: String,
+      enum: ['user', 'admin', 'shelter'],
+      default: 'user',
+      required: true,
     },
-  ],
-},
-{
-  collection: "Users",
-  versionKey: false,
-  timestamps: true,
-  transform: (doc, ret) => {
-    delete ret.password;
-    return ret;
+    phoneNumber: {
+      type: String,
+      required: false,
+    },
+    googleId: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+    petPreferences: petPreferencesSchema,  // Added pet preferences
+    adoptionApplications: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Pet",
+      },
+    ],
+    questionnaires: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Questionnaire",
+      },
+    ],
+    myFavorites: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Pet",
+      },
+    ],
   },
-}
+  {
+    collection: "Users",
+    versionKey: false,
+    timestamps: true,
+    transform: (doc, ret) => {
+      delete ret.password;
+      return ret;
+    },
+  }
 );
 
 export default mongoose.model("User", userSchema, "Users");
