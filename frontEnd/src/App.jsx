eeaf3c661be03a5afff63a5adbdc911ffae605c0
sogol mofrad <sogolmofrad@sogols-iMac.local>
2024-11-23 08:07:08 +0100

    add userAuth context with login and logout functionality

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -11,25 +11,28 @@ import AppLayout from "./components/AppLayout";
 import Pet from "./pages/Pet";
 // import Cat from "./pages/Cat";
 import Registration from "./pages/Registration";
+import { UserAuthProvider } from "./contexts/UserAuthContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/pets" element={<Pets />} />
-          <Route path="/pets/:id" element={<Pet />} />
-          {/* <Route path="/cats/:id" element={<Cat />} /> */}
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Registration />} />
-          <Route path="/adoption" element={<Adoption />} />
-          <Route path="/account" element={<PersonalAccount />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <UserAuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/pets" element={<Pets />} />
+            <Route path="/pets/:id" element={<Pet />} />
+            {/* <Route path="/cats/:id" element={<Cat />} /> */}
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Registration />} />
+            <Route path="/adoption" element={<Adoption />} />
+            <Route path="/account" element={<PersonalAccount />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </UserAuthProvider>
   );
 }
 
