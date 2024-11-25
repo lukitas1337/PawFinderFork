c79bd211e8943e0d3f4bb6c5f750de7552cd4489
Lukitas <lvd.follert@gmail.com>
2024-11-25 11:08:04 +0100

    pref, quest, and favs added to login res

--- backEnd/middleware/authMiddleware.js
+++ backEnd/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 const auth = async (req, res, next) => {
     try {
         const token = req.cookies.token;
-        
+        console.log(token);
         if (!token) {
             return res.status(401).json({ 
                 success: false, 
