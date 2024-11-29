7d78c32a48e5901e03d6f859d1584b5beebc1534
sogol mofrad <sogolmofrad@sogols-iMac.local>
2024-11-29 16:30:34 +0100

    fix minor issues part one

--- frontEnd/src/components/AboutMatchingSection.jsx
+++ frontEnd/src/components/AboutMatchingSection.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 
 const AboutMatchingSection = () => (
-  <section className="container mx-auto px-[40px] lg:px-[120px] flex 
+  <section
+    className="container mx-auto px-[40px] lg:px-[120px] flex 
     flex-col md:flex-row items-center mt-20 md:mt-40 lg:mt-40 xl:mt-40 sm:mt-20 mb-20 sm:mb-20 md:mb-44 
-    lg:mb-44 xl:mb-44 gap-8">
-
+    lg:mb-44 xl:mb-44 gap-8"
+  >
     <div className="flex-1 order-1 sm:order-1 md:order-2 text-center sm:text-center md:text-right">
-      <h2 className="text-dark font-black font-poppins leading-[30px] sm:leading-[30px] 
+      <h2
+        className="text-dark font-black font-poppins leading-[30px] sm:leading-[30px] 
         md:leading-[50px] lg:leading-[60px] xl:leading-[60px] 
-        text-[30px] sm:text-[30px] md:text-[40px] lg:text-[50px] xl:text-[50px] mb-4">
+        text-[30px] sm:text-[30px] md:text-[40px] lg:text-[50px] xl:text-[50px] mb-4 text-left"
+      >
         OUR MATCHING <br /> SYSTEM QUICKLY <br /> FINDS THE PET
       </h2>
-      <p className="text-dark text-[14px] sm:text-[14px] md:text-[16px] lg:text-[18px] xl:text-[18px] 
-      mb-2 sm:mb-2 md:mb-6 lg:mb-6 xl:mb-6">
-        Our advanced matching system ensures you find the ideal pet quickly and effortlessly. By analyzing your information and questionnaire responses, we use innovative technology to match you with pets whose needs and characteristics align perfectly with your preferences. With the detailed data provided by shelters, our system helps create meaningful connections, giving you and your future pet the best chance at a happy life together!
+      <p
+        className="text-dark text-[14px] sm:text-[14px] md:text-[16px] lg:text-[18px] xl:text-[18px] 
+      mb-2 sm:mb-2 md:mb-6 lg:mb-6 xl:mb-6 text-justify"
+      >
+        Our advanced matching system ensures you find the ideal pet quickly and
+        effortlessly. By analyzing your information and questionnaire responses,
+        we use innovative technology to match you with pets whose needs and
+        characteristics align perfectly with your preferences. With the detailed
+        data provided by shelters, our system helps create meaningful
+        connections, giving you and your future pet the best chance at a happy
+        life together!
       </p>
     </div>
 
--- frontEnd/src/components/AboutUsSection.jsx
+++ frontEnd/src/components/AboutUsSection.jsx
@@ -1,28 +1,36 @@
 const AboutUsSection = () => (
-    <section className="container mx-auto px-[40px] lg:px-[120px] flex 
+  <section
+    className="container mx-auto px-[40px] lg:px-[120px] flex 
     flex-col md:flex-row items-center mt-20 sm:mt-20 lg:mt-40 md:mt-40 xl:mt-40 mb-16 md:mb-40 lg:mb-40 
-    xl:mb-40 sm:mb-16 gap-8">
-   
-      <div className="flex-1 order-1 md:order-none text-center md:text-left">
-        <h1 className="text-dark font-black font-poppins leading-[30px] sm:leading-[30px] md:leading-[50px] 
-        lg:leading-[60px] xl:leading-[60px] text-[30px] sm:text-[30px] md:text-[40px] lg:text-[50px] xl:text-[50px] mb-4">
-          PAWFINDER - <br /> A PLACE TO FIND <br /> YOUR PET
-        </h1>
-        <p className="text-dark text-[14px] sm:text-[14px] md:text-[16px] lg:text-[18px] xl:text-[18px] 
-        mb-2 sm:mb-2 md:mb-6 lg:mb-6 xl:mb-6">
-          – your trusted platform for finding pets across shelters in Germany. Browse animals from various shelters, explore detailed profiles, and let our AI match you with the perfect companion. With PawFinder, you can easily apply for adoption and connect with shelters to give a pet a loving home!
-        </p>
-      </div>
-  
-      <div className="flex-1 flex justify-center order-2 md:order-none">
-        <img
-          src="/images/dog_pic_about_us.png"
-          alt="Dog"
-          className="w-[300px] md:w-[400px] h-auto transform rotate-0 md:rotate-6 lg:rotate-6 xl:rotate-6 xl:-mr-48"
-        />
-      </div>
-    </section>
-  );
-  
-  export default AboutUsSection;
-  
+    xl:mb-40 sm:mb-16 gap-8"
+  >
+    <div className="flex-1 order-1 md:order-none text-center md:text-left">
+      <h1
+        className="text-dark font-black font-poppins leading-[30px] sm:leading-[30px] md:leading-[50px] 
+        lg:leading-[60px] xl:leading-[60px] text-[3rem] sm:text-[30px] md:text-[40px] lg:text-[50px] xl:text-[50px] mb-4"
+      >
+        PAWFINDER - A PLACE TO FIND YOUR PET
+      </h1>
+      <p
+        className="text-dark text-[14px] sm:text-[14px] md:text-[16px] lg:text-[18px] xl:text-[18px] 
+        mb-2 sm:mb-2 md:mb-6 lg:mb-6 xl:mb-6 text-justify"
+      >
+        – your trusted platform for finding pets across shelters in Germany.
+        Browse animals from various shelters, explore detailed profiles, and let
+        our AI match you with the perfect companion. With PawFinder, you can
+        easily apply for adoption and connect with shelters to give a pet a
+        loving home!
+      </p>
+    </div>
+
+    <div className="flex-1 flex justify-center order-2 md:order-none">
+      <img
+        src="/images/dog_pic_about_us.png"
+        alt="Dog"
+        className="w-[300px] md:w-[400px] h-auto transform rotate-0 md:rotate-6 lg:rotate-6 xl:rotate-6 xl:-mr-48"
+      />
+    </div>
+  </section>
+);
+
+export default AboutUsSection;
