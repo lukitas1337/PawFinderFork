cf220c593ddfed9100abe0e26ed913813d1d1b92
sogol mofrad <sogolmofrad@gmail.com>
2024-12-02 11:48:27 +0100

    make user page responsive

--- frontEnd/src/pages/PersonalAccount.jsx
+++ frontEnd/src/pages/PersonalAccount.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Outlet } from "react-router-dom"; 
-import AccountSidebar from "../components/AccountSidebar"; 
+import { Outlet } from "react-router-dom";
+import AccountSidebar from "../components/AccountSidebar";
 
 export default function PersonalAccount() {
   return (
-    <div className="flex">
-        <AccountSidebar />
+    <div className="flex flex-col items-center lg:flex-row lg:items-stretch">
+      <AccountSidebar />
       <div className="flex-1 flex flex-col">
         <Outlet />
       </div>
