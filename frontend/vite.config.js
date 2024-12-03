6d5b173550d00e64cc1eb648175d3e677a82a8e7
Lukitas <lvd.follert@gmail.com>
2024-12-03 14:26:58 +0100

    config: update vite config to properly resolve react-leaflet dependencies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['react-leaflet', 'leaflet']
+  resolve: {
+    dedupe: ['react-leaflet', 'leaflet', 'react', 'react-dom'],
+    alias: {
+      'react-leaflet': 'react-leaflet/dist/react-leaflet.js',
+      'leaflet': 'leaflet/dist/leaflet.js'
+    }
   }
 })
